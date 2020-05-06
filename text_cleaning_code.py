041af6c9e9296bfd4263acc16590b61fec13b508
sanjeev <sksanjeevkumar671@gmial.com>
2020-05-06 22:28:40 +0530

    fashion_classificaton kaggle added and some othe new file

--- text_cleaning_code.py
+++ text_cleaning_code.py
@@ -1,6 +1,7 @@
 import re
-s = "how much for the maple $  syrup? ^$20.99? (That's) {ff} [hfhd] |\ridiculous!!!"
-remove_symbol = ['~','`','!','@','#','\$','%', '\^','&','\*','\?',':',';', '/','\|','\(','\)', '{','}','\[','\]','  ']
+s = "how much for the maple_ $  syr-up? ^$20.99? (That's) {ff} [hfhd] |\ridiculous!!!"
+remove_symbol = ['~','`','!','@','#','\$','%', '\^','&','\*','\?',':',';',
+                 '_','-','/','\|','\(','\)', '{','}','\[','\]','  ']
 for sym in remove_symbol:
     print(sym)
     s=re.sub(sym,'',s)
